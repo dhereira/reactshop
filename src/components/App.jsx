b5ca34997052ffd7372bd3e197d6a477e9716eb9
dhereira <davidhereira@gmail.com>
2022-09-05 18:01:10 -0500

    Clase 6 del curso practico de react.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-import React, { useImperativeHandle } from 'react';
+import React from 'react';
 
 const App = () => {
     return (
-       <h1> Hola mundo! </h1>
+       <h1> Hola mundo!!!! </h1>
     );
 }
 
