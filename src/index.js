424e33127d15b151137ab2d7cb9a98185f4589c5
dhereira <davidhereira@gmail.com>
2022-09-15 19:46:54 -0500

    clase 28 de curso practico de react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './components/App';
-
+import  App  from './routes/App';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
 
-root.render(<App/>);
+root.render(<App tab="home" />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js',
+        publicPath: '/',
     },
     mode: 'development',
     resolve:{
-        extensions: ['.js', '.jsx'],        
+        extensions: ['.js', '.jsx'], 
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components/'),
+            '@containers': path.resolve(__dirname, 'src/containers/'),
+            '@pages': path.resolve(__dirname, 'src/pages/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@icons': path.resolve(__dirname, 'src/assets/icons/'),
+            '@logos': path.resolve(__dirname, 'src/assets/logos/'),
+        }       
     },
     module:{
         rules: [
@@ -30,13 +41,44 @@ module.exports = {
                     }
 
                 ]
+            },
+            {
+                test:/\.(css|s[ac]ss)$/i,
+                use: [
+                    devMode ? 'style-loader': MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test:/\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                      loader: 'file-loader',
+                      options: {
+                        name: 'images/[hash]-[name].[ext]',
+                      },
+                    },
+                  ],
             }
-        ]
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: 'index.html'
-        })
-    ]
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+    devServer:{
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9009,
+        historyApiFallback: true
+    },
 }
