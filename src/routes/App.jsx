84340f1cbeccee96ad92fe7b8446cff6149082d7
dhereira <davidhereira@gmail.com>
2022-09-30 13:54:11 -0500

    error con basename en browserouter

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   const initialState = useInitialState();
     return (
       <AppContext.Provider value ={initialState}>
-      <BrowserRouter basename='/reactPractico'>
+      <BrowserRouter basename='/reactshop'>
         <Layout>
           
           <Routes>
