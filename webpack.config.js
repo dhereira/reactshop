fa41d17a591399d8308f250faf778041cc4ac49f
dhereira <davidhereira@gmail.com>
2022-09-30 20:53:33 -0500

    modifique package.jspn y wepack.config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname,'dist'),
-        filename: 'reactshop/bundle.js',
+        filename: 'bundle.js',
         publicPath: './',
     },
     mode: 'development',
