f0057ad8d1a33d21db7e53d91edf60f095439ab2
dhereira <davidhereira@gmail.com>
2022-10-03 18:12:03 -0500

    error en el basename del brownser

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   const initialState = useInitialState();
     return (
       <AppContext.Provider value ={initialState}>
-      <BrowserRouter basename='reactshop'>
+      <BrowserRouter basename='/reactshop'>
         <Layout>
           
           <Routes>
