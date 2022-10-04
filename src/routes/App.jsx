49d0ec99c059de675e5307e7b38995af3849f56c
dhereira <davidhereira@gmail.com>
2022-10-03 20:23:37 -0500

    error en declaracion de Routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout  from '@containers/Layout';
 import  Login  from '@pages/Login';
 import SendEmail from '@pages/SendEmail';
